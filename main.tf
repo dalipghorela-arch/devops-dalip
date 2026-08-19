b68fc9877bf7a44f001473704ccfdd7a8d8af799
Devopsgirish <dalipghorela@gmail.com>
2026-08-19 23:02:47 +0530

    code modified

--- main.tf
+++ main.tf
@@ -5,3 +5,11 @@ resource "azurerm_resource_group" "rgs" {
     resource "azurerm_resource_group" "rgs" {
     name = "bhagwant"
     location = "central india"  }
+
+    resource "azurerm_resource_group" "rgs" {
+    name = "soni"
+    location = "central india"  }
+
+    resource "azurerm_resource_group" "rgs" {
+    name = "dog"
+    location = "central india"  }
